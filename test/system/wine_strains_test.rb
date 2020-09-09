ef9ab47e7943693535da03e54b1c9f7133f42b93
Sebastián Vidal <sebastian@intercode.cl>
2020-09-09 12:57:15 -0300

    pre-multi relation
    Signed-off-by: Sebastián Vidal <sebastian@intercode.cl>

---
+++ test/system/wine_strains_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class WineStrainsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit wine_strains_url
+  #
+  #   assert_selector "h1", text: "WineStrain"
+  # end
+end
